c25cefa55a5f1371dd3ef101f59ce93bb37a95c3
Peter N <peterNelson1@proton.me>
2022-10-19 17:34:01 -0800

    Update PlayerController.cs

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    Vector3 mousePosition;
+    [SerializeField] private float moveSpeed = 0.1f;
+    Rigidbody rb;
+    Vector3 position = new Vector3(0f, 0f, 0f);
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        mousePosition = Input.mousePosition;
+        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+        position = Vector3.Lerp(transform.position, mousePosition, moveSpeed);
     }
 }
