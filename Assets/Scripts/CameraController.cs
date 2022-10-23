56265975f8f2f934ceb93a2a6fad25e3c790eda7
unknown <dch@uoregon.edu>
2022-10-22 21:59:30 -0700

    Integrated Player/Camera Controller
    - moved alot of logic to playercontroller
    - updated player movement calculations
    - camera controller with constant horizontal movement, speed dependent on player position
    - player sprint system with stamina recharge/drain
    - change gameplay values with gamecontroller, positional values with worldcontroller

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    GameController gameController;
+    WorldController worldController;
+
+    Transform t;
+
+    float baseSpeed;
+    float minPlayerMultiplier;
+    float maxPlayerMultiplier;
+
+    private void Awake() {
+        gameController = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameController>();
+        worldController = GameObject.FindGameObjectWithTag("GameController").GetComponent<WorldController>();
+
+        t = gameObject.transform;
+
+        baseSpeed = gameController.GetCameraBaseSpeed();
+        minPlayerMultiplier = gameController.GetCameraMinPlayerMultipier();
+        maxPlayerMultiplier = gameController.GetCameraMaxPlayerMultipier();
+
+        t.position = worldController.GetCameraStartingLocation();
+    }
+
+    private void Update() {
+        t.Translate(Vector3.right * GetCameraSpeed() * Time.deltaTime, Space.World);
+    }
+
+    private float GetCameraSpeed()
+    {
+        float playerScreenX = Camera.main.WorldToScreenPoint(worldController.GetPlayerLocation()).x;
+        float playerFactor = Utilities.Map(playerScreenX, 0, Camera.main.pixelWidth, minPlayerMultiplier, maxPlayerMultiplier);
+        return baseSpeed * playerFactor;
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,26 +4,133 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    GameController gameController;
+    WorldController worldController;
+
+    // Checkpoint
+    private Checkpoint lastCheckpoint = null;
+    
+    // Movement
+    Rigidbody2D rb;
     Vector3 mousePosition;
-    [SerializeField] private float moveSpeed = 0.1f;
-    Rigidbody rb;
-    Vector3 position = new Vector3(0f, 0f, 0f);
-    // Start is called before the first frame update
-    void Start()
+    Vector3 direction;
+    float move;
+    float baseSpeed;
+    float speed;
+    bool sprinting = false;
+
+    // Stats
+    int health;
+    float stamina;
+    float staminaRechargeRate;
+    float staminaDrainRate;
+    float sprintMultplier;
+
+    void Awake()
     {
-        rb = GetComponent<Rigidbody>();
+        gameController = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameController>();
+        worldController = GameObject.FindGameObjectWithTag("GameController").GetComponent<WorldController>();
+
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Start() {
+        // Set stats
+        baseSpeed = gameController.GetPlayerSpeed();
+        sprintMultplier = gameController.GetPlayerSprintMultiplier();
+        staminaRechargeRate = gameController.GetPlayerStaminaRechargeRate();
+        staminaDrainRate = gameController.GetPlayerStaminaDrainRate();
+        ResetStats(); // health and stamina
+
+        // Initial movement
+        rb.MovePosition(worldController.GetPlayerStartingLocation());
     }
 
     // Update is called once per frame
     void Update()
     {
-        mousePosition = Input.mousePosition;
-        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
-        position = Vector3.Lerp(transform.position, mousePosition, moveSpeed);
+        mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePosition = new Vector3(mousePosition.x, mousePosition.y, transform.position.z);
+        direction = (mousePosition - transform.position).normalized;
+        speed = GetCurrentSpeed() * Vector3.Distance(mousePosition, transform.position) * Time.deltaTime;
+        rb.MovePosition(transform.position + (direction * speed));
+        HandleStamina();
     }
 
-    private void FixedUpdate()
+    private void OnCollisionEnter2D(Collision2D other) {
+        GameObject otherObj = other.gameObject;
+        if (otherObj.GetComponent<Checkpoint>())
+        {
+            Checkpoint point = otherObj.GetComponent<Checkpoint>();
+            if (!point.IsActivated()) 
+            {
+                lastCheckpoint = point;
+                point.Activate();
+            }
+        }
+        if (otherObj.GetComponent<EnemyController>())
+        {
+            EnemyController enemy = otherObj.GetComponent<EnemyController>();
+            // health -= enemy.attack;
+            if (health <= 0) OnDeath();
+        }
+    }
+
+    private void OnDeath()
     {
-        rb.MovePosition(position);
+        ResetStats();
+        Respawn();
+    }
+
+    private void Respawn()
+    {
+        if (lastCheckpoint is null) SetLocation(worldController.GetPlayerStartingLocation());
+        else
+        {
+            Vector3 checkpointLocation = lastCheckpoint.GetLocation();
+            SetLocation(new Vector3(checkpointLocation.x, checkpointLocation.y, worldController.GetPlayerStartingLocation().z));
+        }
+    }
+
+    public void ResetStats()
+    {
+        health = gameController.GetPlayerMaxHealth();
+        stamina = gameController.GetPlayerMaxStamina();
+    }
+
+    public Vector2 GetLocation()
+    {
+        return rb.position;
+    }
+
+    public void SetLocation(Vector3 location)
+    {
+        rb.position = location;
+    }
+
+    private float GetCurrentSpeed()
+    {
+        if (Input.GetMouseButton(0) && CanSprint()) 
+        {
+            sprinting = true;
+            return baseSpeed * sprintMultplier;
+        }
+        else
+        {
+            sprinting = false;
+            return baseSpeed;
+        }
+    }
+
+    private void HandleStamina()
+    {
+        if (sprinting && stamina > 0) stamina -= (staminaDrainRate * Time.deltaTime);
+        else if (stamina < 100) stamina += (staminaRechargeRate * Time.deltaTime);
+    }
+
+    private bool CanSprint()
+    {
+        if (stamina > 0) return true;
+        return false;
     }
 }
